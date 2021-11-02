7e652ccb9c904c4b88587c378e65557e8a9ee356
Dylan Silva <rogdylan98@gmail.com>
2021-11-02 15:39:38 -0500

    random

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const app = express();
 
 app.get('/', (req, res) => {
     res.send('Hello from Express!')
+    const method = req.method;
+    const path = req.path;
+    const random = Math.floor(Math.random()*100);
+    console.log(random);
 })
 
 
