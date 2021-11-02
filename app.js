a36d986f4b145dfff9540de1a2c6ec876f505a86
Christopher Hauser <chauser6@gmail.com>
2021-11-02 17:33:55 -0400

    done!

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.get('/', (req, res) => {
     res.send('Hello from Express!')
 
 })
-app.get(/^\/[\w-]+[^xyz]$/, (req, res) => {
+app.all(/^\/[\w-]+[^xyz]$/, (req, res) => {
     const randomNum = Math.floor(Math.random()*100);
     //console.log(random);
     res.render('index', { method: req.method, path: req.path, randomNum})
@@ -19,6 +19,21 @@ app.get(/^\/(.+)?xyz$/, (req, res) => {
     res.send('That\'s all I wrote.');
 })
 
+app.get(/\/capital-letters\/.+/, (req, res) => {
+    const path = req.path.toUpperCase();
+    console.log(path);
+    const pathArr = path.split('/');
+    console.log(pathArr);
+    res.send(`${pathArr[2]}`);
+})
+
+app.get(/^\/(.+)?\/bio$/, (req, res) => {
+    res.send('Bio');
+})
+
+app.get(/^\/(.+)?\/contact$/, (req, res) => {
+    res.send('Contact');
+})
 
 
 
